cc4bf42d3f6a7e7f18f4307409c1c0ed8f679535
Vastro-lorde <omatsolaseund@gmail.com>
2025-03-08 14:40:14 +0100

    fix elastic search config

--- src/utils/elastic-search-logger.js
+++ src/utils/elastic-search-logger.js
@@ -1,18 +1,12 @@
 const { Client } = require("@elastic/elasticsearch");
 
+
 // Elasticsearch Client
 const esClient = new Client({
   node: process.env.ELASTICSEARCH_HOST || "http://localhost:9200",
   auth: {
     username: process.env.ELASTICSEARCH_USER || "elastic",
     password: process.env.ELASTICSEARCH_PASS || "changeme",
-  },
-  requestTimeout: 30000,
-  maxRetries: 3,
-  sniffOnStart: true,  // Automatically discovers other nodes on startup
-  sniffInterval: 60000, // Refreshes the node list every 60s
-  tls: {
-    rejectUnauthorized: false, // Allow self-signed SSL if needed
   },
 });
 
